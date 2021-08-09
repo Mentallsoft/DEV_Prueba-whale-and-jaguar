a21ffe702133ddd003f5cf899a8b66a81c23b70b
efrain pizarro <efrain.pizarro@uao.edu.co>
2021-08-08 20:16:56 -0500

    Mejorando estilos

--- src/App.js
+++ src/App.js
@@ -2,37 +2,27 @@ import React, { Component } from "react";
 import './App.css';
 import NumberFormat from "react-number-format";
 import api from './components/api';
-import NuevaPagina from './components/barco'
+import BarChart from './components/barchart'
 
 class App extends Component {
 
   state = {
     countriesJSON: [],
-    sortType: 'asc',
-    countries: [],
-    population: []
+    sortType: 'desc'
   }
 
   async componentDidMount() {
+
     await api.get()
-    .then(
-      response=>{
-        var auxRespuesta = response.data;
-        var auxCountries = [];
-        var auxPopulation = [];
+      .then(
+        response => {
+          var auxRespuesta = response.data;
 
-        auxRespuesta.map(element=>{
-          auxCountries.push(element.name);
-          auxPopulation.push(element.population)
-        })
-
-        this.setState({
-          countriesJSON: response.data,
-          countries: auxCountries,
-          population: auxPopulation
-        })
-      }
-    );    
+          this.setState({
+            countriesJSON: auxRespuesta,
+          })
+        }
+      );
   }
 
   directionalSort = sortType => {
@@ -41,30 +31,42 @@ class App extends Component {
 
   render() {
 
-    const { countriesJSON, sortType, countries, population } = this.state;
+    const { countriesJSON, sortType } = this.state;
 
-    const sorted = countriesJSON.sort((a, b) => {
+    const sorted2 = countriesJSON.sort(function (a, b) {
+      const sortedJSON = (sortType === 'asc') ? a.population - b.population : b.population - a.population;
+      return sortedJSON;
+    });
+
+    var auxCountries = [];
+    var auxPopulation = [];
+
+    sorted2.map(element => {
+      auxCountries.push(element.name);
+      auxPopulation.push(element.population)
+    })
+
+    /*const sorted = countriesJSON.sort((a, b) => {
       const isReversed = (sortType === 'asc') ? 1 : -1;
       return isReversed * a.name.localeCompare(b.name)
-    });
+    });*/
 
     return (
       <div className="o-app-container">
         <h1>
           COUNTRIES
         </h1>
-        <NuevaPagina olabels={countries} ovalues={population}/>
-        <br/>
-        <br />
-        <div className='row'>
         <div className='o-container-buttons'>
           <button className='o-button' disabled={(sortType === 'asc') ? true : false} onClick={() => this.directionalSort('asc')}>Sort by asc</button>
           <button className='o-button' disabled={(sortType === 'desc') ? true : false} onClick={() => this.directionalSort('desc')}>Sort by desc</button>
         </div>
-        <br/>
-        <br/>
-        <br/>
-          {sorted.map(
+        <br />
+        <BarChart olabels={auxCountries} ovalues={auxPopulation} />
+        <br />
+        <br />
+        <div className='row'>
+
+          {sorted2.map(
             (countryJSON, key) => (
               <div className="o-card" key={key}>
                 <h2 id="o-country">
